851cc175b398034c1b12915960870897dec28cfc
Giorgos Iliopoulos <giorgos.iliopoulos97@gmail.com>
2021-10-20 22:25:58 +0300

    Cors basics

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
 
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
 
   // Pass to next layer of middleware
   next();
