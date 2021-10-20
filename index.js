051c37e527f169a052c6175f6924a5516fe36395
Giorgos Iliopoulos <giorgos.iliopoulos97@gmail.com>
2021-10-20 22:25:03 +0300

    Cors from everywhere

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 var database, championships;
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get('/', function (req, res) {
   res.send(console.log('SuperleagueGreeceApi by Giorgos Iliopoulos'))
 })
